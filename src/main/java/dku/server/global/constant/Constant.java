be56c378f77768776319d1d18b05c3f9fa1ee422
Yun Seongmin <ilcm96@gmail.com>
2025-06-09 20:25:01 +0900

    refactor: 프로덕션 클라이언트 URL 추가

--- src/main/java/dku/server/global/constant/Constant.java
+++ src/main/java/dku/server/global/constant/Constant.java
@@ -9,9 +9,12 @@ public class Constant {
     public static final String LOCAL_CLIENT_URL = "http://localhost:5173";
     public static final String LOCAL_CLIENT_URL_BUILD = "http://localhost:4173/";
 
+    public static final String PROD_CLIENT_URL = "https://dku-java-3-client.seongmin.dev";
+
     public static final List<String> ALLOWED_CLIENT_URLS = List.of(
             LOCAL_SERVER_URL,
             LOCAL_CLIENT_URL,
-            LOCAL_CLIENT_URL_BUILD
+            LOCAL_CLIENT_URL_BUILD,
+            PROD_CLIENT_URL
     );
 }
