971627b8a77773f494cf82695c001ebd68023059
Yun Seongmin <ilcm96@gmail.com>
2025-06-09 14:42:07 +0900

    refactor: Vite 관련 URL을 ALLOWED_CLIENT_URLS에 추가

--- src/main/java/dku/server/global/constant/Constant.java
+++ src/main/java/dku/server/global/constant/Constant.java
@@ -6,7 +6,12 @@ public class Constant {
 
     public static final String LOCAL_SERVER_URL = "http://localhost:8080";
 
+    public static final String LOCAL_CLIENT_URL = "http://localhost:5173";
+    public static final String LOCAL_CLIENT_URL_BUILD = "http://localhost:4173/";
+
     public static final List<String> ALLOWED_CLIENT_URLS = List.of(
-            LOCAL_SERVER_URL
+            LOCAL_SERVER_URL,
+            LOCAL_CLIENT_URL,
+            LOCAL_CLIENT_URL_BUILD
     );
 }
